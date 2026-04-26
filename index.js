0cfbbad1cf2fe6e2bf3cb431ef3aa729e9e5d452
devanshythakkar-pixel <devanshythakkar@gmail.com>
2026-04-26 14:12:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,32 @@ let bot;
 
 function createBot() {
     bot = mineflayer.createBot(botArgs);
-    bot.on('login', () => console.log('SUCCESS: Bot is in the server!'));
+
+    bot.on('login', () => {
+        console.log('SUCCESS: Bot is in the server!');
+        
+        // This runs every 20 seconds (20,000 milliseconds)
+        setInterval(() => {
+            console.log('Bot is performing 4 jumps...');
+            
+            let jumps = 0;
+            const jumpInterval = setInterval(() => {
+                bot.setControlState('jump', true);
+                bot.setControlState('jump', false);
+                jumps++;
+
+                if (jumps >= 4) {
+                    clearInterval(jumpInterval);
+                }
+            }, 500); // Wait 0.5 seconds between each jump so they look real
+            
+        }, 20000); 
+    });
+
     bot.on('end', () => {
         console.log('Bot disconnected. Retrying in 30s...');
         setTimeout(createBot, 30000);
     });
+
     bot.on('error', (err) => console.log('Error Type:', err.code));
 }
-
-createBot();
